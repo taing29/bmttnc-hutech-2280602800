67e250746d46baf8d2c0c4bc5d5455a93e82cd90
taing <taiflunf@gmail.com>
2025-06-19 09:58:17 +0700

    xong ktra

--- TEST/app.py
+++ TEST/app.py
@@ -1,15 +1,19 @@
 import subprocess
 from flask import Flask, render_template, request, json
-# from cipher.caesar import CaesarCipher
-# from cipher.vigenere import VigenereCipher
-# from cipher.railfence import RailFenceCipher
-# from cipher.playfair import PlayFairCipher
+from flask import send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route('/TEST/<filename>')
+def serve_test_file(filename):
+    try:
+        return send_from_directory('TEST', filename)
+    except FileNotFoundError:
+        return f"File {filename} not found in TEST", 404
 
 @app.route("/caesar")
 def open_caesar_gui():
